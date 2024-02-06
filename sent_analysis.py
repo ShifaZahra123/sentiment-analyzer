109d3631edaea787dcde26ec788d618503505a96
ShifaZahra123 <140159309+ShifaZahra123@users.noreply.github.com>
2024-02-06 17:31:19 +0500

    Add files via upload

---
+++ sent_analysis.py
@@ -0,0 +1,35 @@
+import openai
+import streamlit as st
+from pathlib import Path
+import configparser
+
+
+cfg_reader = configparser.ConfigParser()
+fpath = Path.cwd() / Path('config.ini')
+cfg_reader.read(str(fpath))
+openai.api_key = st.secrets['OPENAI_API_KEY']
+
+def get_response_from_chatgpt(text):
+    prompt= "Identify and return the sentiment either positive or negative in given text. text: {}".format(text)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful Text Sentiment Analyzer That returns concise Sentiment."},
+            {"role": "user", "content": prompt }
+        ],
+        temperature = 0.1
+    )
+    sentiment = response['choices'][0]['message']['content']
+    return sentiment
+
+st.title("ChatGPT Text Sentiment Analyzer")
+model="gpt-3.5-turbo"
+
+text = st.text_input("Enter Text: ", value= "I love to read AI Books")
+
+if st.button('Submit'):
+    with st.spinner('OpenAI Processing in progress'):
+        sentiment = get_response_from_chatgpt(text)
+        st.success('OpenAI Processing Complete')
+
+    st.write("Sentiment: {}".format(sentiment))
